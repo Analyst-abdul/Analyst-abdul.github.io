e811f4fb6215a721de5ccae732ed35dbeadd9257
Analyst-abdul <162406280+Analyst-abdul@users.noreply.github.com>
2024-03-06 21:29:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ timeButton.addEventListener('click', () => {
   const currentTime = new Date().toLocaleTimeString();
   timeDisplay.textContent = `The current time is: ${currentTime}`;
 });
+const downloadBtn = document.querySelector('.download-btn');
+  downloadBtn.addEventListener('click', () => {
+  console.log('Download initiated');
+});
