20f8eec01aa95eabed0cf008d0fa997e92c65fe8
Analyst-abdul <162406280+Analyst-abdul@users.noreply.github.com>
2024-03-06 10:37:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const timeButton = document.getElementById('time-button');
+const timeDisplay = document.getElementById('time-display');
+
+timeButton.addEventListener('click', () => {
+  const currentTime = new Date().toLocaleTimeString();
+  timeDisplay.textContent = `The current time is: ${currentTime}`;
+});
